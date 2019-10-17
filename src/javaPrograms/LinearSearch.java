32f4385fb0c1ece705baf0e028ffc32256a6846e
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-10-18 00:27:39 +0530

    linear search

---
+++ src/javaPrograms/LinearSearch.java
@@ -0,0 +1,26 @@
+package javaPrograms;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+		int a[]= {10,20,40,30,50};
+		int search_ele=50;
+		boolean flag=false;
+		
+		for(int i=0;i<a.length;i++)
+		{
+			if(search_ele==a[i])
+			{
+				System.out.println("element found at index: "+i);
+				flag=true;
+				break;
+				
+			}
+		}
+		if(flag==false)
+		{
+			System.out.println("element not found");
+		}
+	}
+
+}
