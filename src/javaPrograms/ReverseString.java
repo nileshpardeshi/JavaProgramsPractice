7623a7e57a0bf2344b2091bdd9907478e0983f60
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-10-18 00:11:39 +0530

    reverse string

---
+++ src/javaPrograms/ReverseString.java
@@ -0,0 +1,37 @@
+package javaPrograms;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		String str = "ABCD";
+		String rev = "";
+
+		// Logic 1: using charAt method
+		/*
+		 * int len=str.length();
+		 * 
+		 * for(int i=len-1;i>=0;i--) { rev=rev+str.charAt(i); }
+		 * 
+		 * System.out.println("Reverse string is: "+rev);
+		 */
+		
+		//logic 2: using character array
+		
+		/*
+		 * char a[]=str.toCharArray();
+		 * 
+		 * int len=a.length;
+		 * 
+		 * for(int i=len-1;i>=0;i--) { rev=rev+a[i]; }
+		 * System.out.println("Reverse string is: "+rev);
+		 */
+		
+		//logic 3: using string buffer class
+		
+		StringBuffer sb=new StringBuffer(str);
+		System.out.println(sb.reverse());
+		
+	}
+
+}
