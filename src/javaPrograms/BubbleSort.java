dbdf39dc0fb55522a3d241fc875797961f5b32f7
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-10-17 23:09:30 +0530

    Bubble sort pgm added

---
+++ src/javaPrograms/BubbleSort.java
@@ -0,0 +1,32 @@
+package javaPrograms;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		int a[]= {4,2,1,5,3};
+		
+		System.out.println("Before array"+Arrays.toString(a));
+		
+		int n=a.length;
+		
+		for(int i=0; i<n-1; i++)
+		{
+			for(int j=0; j<n-1; j++) 
+			{
+				if (a[j]>a[j+1]) 
+				{
+					int temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+				
+			}
+		}
+		
+		System.out.println("Sorted array is"+Arrays.toString(a));
+	}
+
+}
