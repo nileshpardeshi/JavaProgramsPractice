a900b681ac3d5c795f43c2933e4ca8b947a8f6f2
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-10-19 16:02:56 +0530

    CountNumberofDigits

---
+++ src/javaPrograms/CountNumberofDigits.java
@@ -0,0 +1,24 @@
+package javaPrograms;
+
+import java.util.Scanner;
+
+public class CountNumberofDigits {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number :");
+		int num=sc.nextInt();
+		int count=0;
+		
+		while(num>0)
+		{
+			num=num/10;
+			count++;
+		}
+		
+		System.out.println("Total count is :"+count);
+		
+	}
+
+}
